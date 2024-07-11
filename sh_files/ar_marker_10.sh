5137d9311de423a410a3081a3f04b52913d22e9e
johny <johny@hyulimnetworks.com>
2024-07-11 13:26:16 +0900

    ★ ar 마커 스크립트 추가

---
+++ sh_files/ar_marker_10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /opt/ros/melodic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+source ~/cartographer_ws/devel_isolated/setup.bash
+source ~/.bashrc
+echo $ROS_NAMESPACE
+export ROS_NAMESPACE=TETRA_NS
+
+pkill -9 -f ar_marker_20.sh
+sleep 1
+
+roslaunch tetraDS_2dnav ar_marker_10.launch

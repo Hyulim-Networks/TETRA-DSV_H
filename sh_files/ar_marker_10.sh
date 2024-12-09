6fd78a0a7f436605db3520cc28842a67f51dac37
johny <johny@hyulimnetworks.com>
2024-12-09 10:13:45 +0900

    ♥ 맵핑시 imu, scan 워닝 제거, 인터페이스 안쓰는 변수 제거, 소싱 순서 변경

--- sh_files/ar_marker_10.sh
+++ sh_files/ar_marker_10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-source ~/catkin_ws/devel/setup.bash
 source ~/cartographer_ws/devel_isolated/setup.bash
+source ~/catkin_ws/devel/setup.bash
 source ~/.bashrc
 echo $ROS_NAMESPACE
 export ROS_NAMESPACE=TETRA_NS
